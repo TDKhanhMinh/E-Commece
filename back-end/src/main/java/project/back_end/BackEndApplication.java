24a0c9f5c6cfde2625438678fa1df1c2774147c9
Tran Minh <trandokhanhminh@gmail.com>
2026-03-27 10:11:30 +0700

    fix create default admin

--- back-end/src/main/java/project/back_end/BackEndApplication.java
+++ back-end/src/main/java/project/back_end/BackEndApplication.java
@@ -1,15 +1,26 @@
 package project.back_end;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import project.back_end.service.UserService;
 
 @SpringBootApplication
 public class BackEndApplication {
-private static UserService userService;
+
     public static void main(String[] args) {
-        userService.createAdminUserIfNotExist();
+
         SpringApplication.run(BackEndApplication.class, args);
     }
+    @Bean
+    CommandLineRunner initDatabase(UserService userService) {
+        return args -> {
+            System.out.println("Đang kiểm tra Admin...");
+            userService.createAdminUserIfNotExist();
+            System.out.println("Hoàn tất kiểm tra Admin!");
+        };
+    }
+
 
 }
