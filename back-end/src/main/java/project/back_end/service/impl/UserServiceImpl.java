71bb82b2e024ea1a27bcf85e8281d289178b2919
Tran Minh <trandokhanhminh@gmail.com>
2026-03-30 09:22:02 +0700

    update secret key

--- back-end/src/main/java/project/back_end/service/impl/UserServiceImpl.java
+++ back-end/src/main/java/project/back_end/service/impl/UserServiceImpl.java
@@ -83,7 +83,6 @@ public class UserServiceImpl implements UserService {
     savedUser.setCreatedAt(LocalDateTime.now());
     savedUser.setAvatarUrl("https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=");
     userRepo.save(savedUser);
-    log.info("User registered successfully: {}", savedUser.getEmail());
     return "Admin user created successfully.";
 }
     @Override
